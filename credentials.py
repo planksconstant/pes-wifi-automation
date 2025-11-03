17be7af4e1a170b44930c2c842ab5e3839223409
planksconstant <abhishekdharanisha@icloud.com>
2025-11-03 16:04:03 +0530

    issue#1 solved

--- credentials.py
+++ credentials.py
@@ -2,11 +2,13 @@
 import os
 import platform
 platform = platform.system()
+
 class login():
-    def conifg(self):
+
+    def config(self):
         a = open("credentials.txt", "r")
-        if a.readlines() == []:
-            a = open("credentials.txt", "w")
+        if a.readlines() != []:
+            a = open("credentials.txt", "a")
             if platform=="Darwin":#Darwin is mac
                 print("Looks like you are on Mac OS ")
                 print("Are you on an ARM processor? (Apple M Series Chip)")
@@ -22,19 +24,24 @@ class login():
                 a.write("chromedriver_win64.exe")
             else:
                 print("Looks like you are on an unsupported platform :( ")
+        else:
+            print("Configuration Issue Contact @planksconstant ")
+            global login_issue
+
 
 
 
     def accept_name_password(self):
-        a=open("credentials.txt","r")
-        if a.readlines()==[]:
-            a = open("credentials.txt", "w")
-            srn=input("Enter SRN : ")
-            pwd=input("Enter Password : ")
-            a.write(srn)
-            a.write("\n")
-            a.write(pwd)
-            a.write("\n")
-        else:
-            print ("Credentials Exist")
+
+        a = open("credentials.txt", "w")
+        srn=input("Enter SRN : ")
+        pwd=input("Enter Password : ")
+        a.write(srn)
+        a.write("\n")
+        a.write(pwd)
+        a.write("\n")
+
+
 login().accept_name_password()
+login().config()
+
