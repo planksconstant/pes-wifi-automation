75f11d0adb986a3d11fda4601b659f415f96428a
planksconstant <abhishekdharanisha@icloud.com>
2025-11-07 17:01:14 +0530

    added coplete working frame with main.py

--- login.py
+++ login.py
@@ -21,19 +21,22 @@ def read():
     credentials_list=a.readlines()
     return credentials_list
 def automator():
-    x=read()
-    driver = setup_drive()
-    username = x[0]
-    password = x[1]
-    driver.get("http://192.168.254.1:8090/httpclient.html")
-    time.sleep(0.5)#required to make sure the fields do not get bad text
-    driver.find_element(By.NAME, "username").send_keys(username)
-    driver.find_element(By.NAME, "password").send_keys(password)
-    driver.find_element(By.ID, "loginbutton").click()
-    time.sleep(0.5)
-    driver.quit()
+    try:
+        x=read()
+        driver = setup_drive()
+        username = x[0]
+        password = x[1]
+        driver.get("http://192.168.254.1:8090/httpclient.html")
+        time.sleep(0.5)#required to make sure the fields do not get bad text
+        driver.find_element(By.NAME, "username").send_keys(username)
+        driver.find_element(By.NAME, "password").send_keys(password)
+        driver.find_element(By.ID, "loginbutton").click()
+        time.sleep(0.5)
+    finally:
+        driver.quit()
 
-automator()
+
+
 
 #login failed ==> <selenium.webdriver.remote.webelement.WebElement (session="9F63AA64-D1DE-45AA-B979-E37440C374B3", element="node-7AF1754A-1D40-44D0-B696-DA020798E7DF")>
 #login success==> <selenium.webdriver.remote.webelement.WebElement (session="F821B13F-A9BC-4EDC-A196-2D2D8370E7DF", element="node-CCEE84C6-F0E9-417E-97E6-91615926F273")>
