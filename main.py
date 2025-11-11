baa99fd8dc90c870573bab6b4d1c78e33f3abe62
planksconstant <abhishekdharanisha@icloud.com>
2025-11-11 10:40:31 +0530

    removed connect to wifi feature until#12 works

--- main.py
+++ main.py
@@ -1,14 +1,12 @@
 import credentials
-import os
-if os.path.exists('credentials.txt'):
-    credentials.accept_name_password()
 
 import time
 import login
 import clear_stored_data
-import connect_to_wifi
+#import connect_to_wifi
 print("Running in review mode all stored data will be cleared :) ")
-connect_to_wifi.connecting_wifi()
-time.sleep(0.5)
+credentials.accept_name_password()
+#connect_to_wifi.connecting_wifi()
+#time.sleep(0.5)
 login.automator()
 clear_stored_data.clear()
